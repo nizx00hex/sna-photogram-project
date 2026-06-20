36763a8bd2f973def8e68f351e83cece1d648b6c
Nisath <nisathnisath606@gmail.com>
2026-06-20 22:58:54 +0530

    chore: update test script

--- test.php
+++ test.php
@@ -1,10 +1,19 @@
-<pre> 
+<!-- <pre>  -->
     <?php
-    print_r($_SERVER);
-    print_r($_POST);
-    print_r($_GET);
-    print_r($_FILES);
-    print_r($_COOKIE);
+    include "libs/load.php";
+    $username = "nisath0x011";
+    $password = "password333";
+    $email = "n@gmaiil.com";
+    $phone = "124124";
+
+    signup($username, $password, $email, $phone);
+    // print_r($_SERVER);
+    // print_r($_POST);
+    // print_r($_GET);
+    // print_r($_FILES);
+    // print_r($_COOKIE);
     ?>    
-</pre>
+<!-- </pre> -->
+
+
 
