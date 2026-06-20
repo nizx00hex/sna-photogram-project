c20e26cf7ecdeec5d66674aab34c0dd11a264fc8
Nisath <nisathnisath606@gmail.com>
2026-06-20 22:57:11 +0530

    feat(ui): add and update template components

---
+++ _templates/_head.php
@@ -0,0 +1,26 @@
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content="" />
+    <meta
+      name="author"
+      content="selfmade ninja Academy"
+    />
+    <meta name="generator" content="Astro v5.13.2" />
+    <title>Photogram by LHTP</title>
+    <link
+      rel="canonical"
+      href="https://getbootstrap.com/docs/5.3/examples/album/"
+    />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hover.css/2.3.1/css/hover-min.css" integrity="sha512-csw0Ma4oXCAgd/d4nTcpoEoz4nYvvnk21a8VA2h2dzhPAvjbUIK6V3si7/g/HehwdunqqW18RwCJKpD7rL67Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <script src="assets/js/color-modes.js"></script>
+    <link href="assets/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/dist/css/sign-in.css" rel="stylesheet" />
+    <?php
+    if(file_exists($_SERVER['DOCUMENT_ROOT'] . '/assets/dist/css/' . basename($_SERVER['PHP_SELF'], ".php") . ".css")){
+    ?>
+        <link rel="stylesheet" href="assets/dist/css/<?=basename($_SERVER['PHP_SELF'], ".php")?>.css"/>
+    <?php
+    }
+    ?>
+    <meta name="theme-color" content="#712cf9" />
---
+++ _templates/_signup.php
@@ -0,0 +1,120 @@
+<?php
+
+
+$signup = false;
+if(isset($_POST['username']) and isset($_POST['email']) and isset($_POST['phone']) and isset($_POST['password'])) {
+    $username = $_POST['username'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+    $error = signup($username, $password, $email, $phone);
+    // var_dump($error);
+    // exit;
+    $signup = true;
+}
+
+if($signup){
+  ?>
+    <?php 
+    if(!$error) {
+    ?>
+        <main class="container">
+            <div class="bg-body-tertiary p-5 rounded">
+                <h1>Registration Success.</h1>
+                <p class="lead">You can login now. <a href="/login.php">Here.</a></p>
+            </div>
+        </main>
+    <?php        
+    } else {
+    ?>
+        <main class="container">
+            <div class="bg-body-tertiary p-5 rounded">
+                <h1>Registration Failed.</h1>
+                <p class="lead">Something went wrong, <?php echo $error?><a href="/signup.php">Register again.</a></p>
+            </div>
+        </main>
+    <?php
+    }
+    ?>
+
+  <?php
+} else {
+
+?>
+<main class="form-signin w-100 m-auto">
+    <form method="post" action="signup.php"> 
+        <img
+          class="mb-4"
+          src="https://nisath.elitefort.net/fortrix.png"
+          alt=""
+          width="205"
+          height="190"
+        />
+        <h1 class="h3 mb-3 fw-normal">Please sign up</h1>
+        
+        <div class="form-floating">
+          <input
+            type="text"
+            name="username"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Username</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="text"
+            name="phone"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Phone</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="email"
+            name="email"
+            class="form-control"
+            id="floatingInput"
+            placeholder="name@example.com"
+          />
+          <label for="floatingInput">Email address</label>
+        </div>
+
+        <div class="form-floating">
+          <input
+            type="password"
+            name="password"
+            class="form-control"
+            id="floatingPassword"
+            placeholder="Password"
+          />
+          <label for="floatingPassword">Password</label>
+        </div>
+
+        <div class="form-check text-start my-3">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            value="remember-me"
+            id="checkDefault"
+          />
+          <!-- <label class="form-check-label" for="checkDefault">
+            Remember me
+          </label> -->
+        </div>
+        <button class="btn btn-primary w-100 py-2 hvr-grow-rotate" type="submit">
+          Sign up
+          
+        </button>
+        <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2025</p>
+      </form>
+</main>
+<?php
+}
+?>
